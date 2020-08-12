f6f4a1bfdc054bf80fa93e0afa375e3b80767710
Dallan Freemantle <dallan.freemantle@gmail.com>
2020-08-12 22:46:25 +1200

    Update import statements

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-import SeratoParser = require('./serato-parser');
-import RekordboxParser = require('./rekordbox-parser');
+import * as SeratoParser from './serato-parser';
+import * as RekordboxParser from './rekordbox-parser';
 
 export { SeratoParser, RekordboxParser };
